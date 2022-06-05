d9882cd412cbc064cafb4a298974df4a87cdc2cc
Jijie LIU <liujijieseason@gmail.com>
2022-06-05 22:38:21 +0200

    feat: create comments microservices

---
+++ blog/comments/index.js
@@ -0,0 +1,37 @@
+// import packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+// set up express app
+const app = express();
+app.use(bodyParser.json());
+
+// dummy data
+// key: post id
+// value: { comment id, comment content }
+const commentsByPostId = {};
+
+// endpoints
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] ?? []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  // create a new comment
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  // add it to comments
+  const comments = commentsByPostId[req.params.id] ?? [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  // send response
+  res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+// run the microservice
+app.listen(4001, () => {
+  console.log("Comments microservice is running on port 4001.");
+});
